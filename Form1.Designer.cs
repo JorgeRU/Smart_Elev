341f3af6d68e8fd21e9398ff7d47089a074b6b3c
JorgeRU <jorge.rueda@epitech.eu>
2021-11-19 17:09:37 +0100

    Commit Master

---
+++ Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace TP_81
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel6.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel6.Location = new System.Drawing.Point(527, 528);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(184, 121);
+            this.panel6.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel1.Location = new System.Drawing.Point(439, 528);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(184, 121);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(96, 121);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel5.Location = new System.Drawing.Point(88, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(184, 126);
+            this.panel5.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel3.Location = new System.Drawing.Point(524, 362);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(184, 121);
+            this.panel3.TabIndex = 12;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel4.Location = new System.Drawing.Point(524, 193);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(184, 121);
+            this.panel4.TabIndex = 13;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel7.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.panel7.Location = new System.Drawing.Point(523, 27);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(184, 121);
+            this.panel7.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(714, 614);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "M";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(714, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "M";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(714, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "M";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(714, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(795, 279);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "D";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(795, 448);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 35);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "D";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(689, 505);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(687, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(690, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "4";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(80, 40);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 44);
+            this.button7.TabIndex = 26;
+            this.button7.Text = "Etage 4";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(80, 90);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(115, 44);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "Etage 3";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(80, 140);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(115, 44);
+            this.button9.TabIndex = 28;
+            this.button9.Text = "Etage 2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(80, 190);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(115, 44);
+            this.button10.TabIndex = 29;
+            this.button10.Text = "Etage 1";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Location = new System.Drawing.Point(150, 208);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 252);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tableau de commandées";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(173, 466);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(177, 48);
+            this.button12.TabIndex = 32;
+            this.button12.Text = "Quitter";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 676);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Ascenseur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_81
+{
+    public partial class Form1 : Form
+    {
+        bool t1,t2,t3,t4;
+        int Est=0;
+        int broke;
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+            t4 = false;
+            broke = 0;
+            Est = 0;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            t2 = true;
+            if (i > 234)
+            {
+                i--;
+                panel1.Location = new Point(300, i);
+            }
+
+            else if (i < 234)
+            {
+                i++;
+                panel1.Location = new Point(300, i);
+            }
+            else if (i == 234)
+            {
+                Est = 2;
+                timer2.Stop();
+            }
+
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            timer4.Start();
+
+            if (Est == 0)
+            {
+                i = 350;
+            }
+
+           else if (Est == 2)
+            {
+                i = 234;
+            }
+
+            else if (Est == 3)
+            {
+                i = 127;
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (i > 127)
+            { 
+                i--;
+                panel1.Location = new Point(300, i);
+            }
+
+            else if (i < 127)
+            {
+                i++;
+                panel1.Location = new Point(300, i);
+            }
+            else if (i == 127)
+            {
+                Est = 3;
+                timer3.Stop();
+            }
+
+
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+
+            if (Est == 0)
+            {
+                 i = 350;
+            }
+
+            else if (Est == 3) 
+            {
+                i = 124;
+            
+            }
+            else if (Est == 4)
+            {
+                i = 17;
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            timer3.Start(); 
+
+            if (Est == 0)
+            {
+                i = 350;
+            }
+
+            else if (Est == 2)
+            {
+                i = 234;
+            }
+
+            else if (Est == 4)
+            {
+                i = 17;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            t4 = true;
+
+            if (i > 17)
+            {
+                i--;
+                panel1.Location = new Point(300, i);
+            }
+            else if (i == 17)
+            {
+                Est = 4;
+                timer4.Stop();
+                t4 = false;
+                
+            }
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            timer3.Start();
+          
+            if (t4 == true)
+            {
+                timer4.Stop();
+
+                for (i = 234; i > 127; i--)
+                {
+                    panel1.Location = new Point(300, i);
+                    System.Threading.Thread.Sleep(10);
+                   
+                }
+                System.Threading.Thread.Sleep(1500);
+
+                timer4.Start();
+            }
+
+
+
+          
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer4.Start();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer3.Start();
+
+            if (t1 == true)
+            {
+                timer1.Stop();
+
+                for (i = 60; i < 127; i++)
+                {
+                    panel1.Location = new Point(300, i);
+                    System.Threading.Thread.Sleep(10);
+
+                }
+                System.Threading.Thread.Sleep(10000);
+
+                timer1.Start();
+            }
+            else if (t2 == true)
+            {
+                timer2.Stop();
+
+                for (i = 60; i < 127; i++)
+                {
+                    panel1.Location = new Point(300, i);
+                    System.Threading.Thread.Sleep(10);
+
+                }
+                System.Threading.Thread.Sleep(1500);
+
+                timer2.Start();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+
+            if (t4 == true)
+            {
+                timer4.Stop();
+
+                for (i =300 ; i > 234; i--)
+                {
+                    panel1.Location = new Point(300, i);
+                    System.Threading.Thread.Sleep(10);
+
+                }
+
+                System.Threading.Thread.Sleep(2000);
+
+                timer4.Start();
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+
+            if (t1 == true)
+            {
+                timer1.Stop();
+
+                for (i = 150; i < 234; i++)
+                {
+                    panel1.Location = new Point(300, i);
+                    System.Threading.Thread.Sleep(10);
+
+                }
+                System.Threading.Thread.Sleep(1500);
+
+                timer1.Start();
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t1 = true;
+            if (i<345)
+            {
+                i++;
+                panel1.Location = new Point(300, i);
+            }
+            else if (i == 345)
+            {
+                Est = 0;
+                timer1.Stop();
+                t1 = false;
+            }
+        }
+    }
+}
